75c1c863982ec07dfb533e5c3493d09cb23854a3
Dominic Chiampi <dominicchiampi@gmail.com>
2021-08-22 17:04:02 +0000

    fix reactor startup bugs

--- src/muppeteer/core.clj
+++ src/muppeteer/core.clj
@@ -13,7 +13,8 @@
 (defn init-reactor! []
   (let [r (mr/create :dispatcher-type :thread-pool :event-routing-strategy :broadcast)]
     (swap! state assoc :reactor r)
-    (mr/on r (match-all) (fn [event] (pprint event)))))
+    ;; (mr/on r (match-all) (fn [event] (pprint event)))
+    ))
 
 (defn notify [k message]
   (let [r (:reactor @state)]
@@ -35,18 +36,19 @@
      (swap! state assoc bot {:client client :terminate-chan terminate-chan})
      (attach-message-pump client terminate-chan))))
 
-(defn start-bot-sub
-  [event main-token]
-  (if (contains? event :terminate-chan)
-    (start-bot! (:bot event) main-token (:terminate-chan event))
-    (start-bot! (:bot event) main-token)))
+(defn start-bot-sub [event]
+  (let [bot (get-in event [:data :bot])
+        token (get-in config [bot :token])
+        terminate-chan (get-in event [:data :terminate-chan])]
+    (if terminate-chan
+      (start-bot! bot token terminate-chan)
+      (start-bot! bot token))))
 
 (defn init-subscribers []
-  (let [r (:reactor @state)
-        main-token (get-in config [:main :token])]
+  (let [r (:reactor @state)]
     (mr/on-error r Exception (fn [event]
                                (println event)))
-    (mr/on r ($ :start-bot) (fn [event] (start-bot! event main-token)))
+    (mr/on r ($ :start-bot) (fn [event] (start-bot-sub event)))
     (mr/on r ($ :debug-state) (fn [event] (println @state)))))
 
 (defn -main [& args]
@@ -54,5 +56,5 @@
     (init-reactor!)
     (init-subscribers)
     (notify :start-bot {:bot :main :terminate-chan main-terminate})
-    (notify :debug-state {})
+    ;; (notify :debug-state {})
     (<!! main-terminate)))

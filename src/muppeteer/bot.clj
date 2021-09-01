d88af38fa9a36b6ff44d5513b9ba556f4bfe6e41
Dominic Chiampi <dominicchiampi@gmail.com>
2021-09-01 00:09:38 +0000

    remove old code not used after rewrite

--- src/muppeteer/bot.clj
+++ src/muppeteer/bot.clj
@@ -1,28 +1,11 @@
 (ns muppeteer.bot
   (:require [clojure.java.io :as io]
-            [clojure.core.async :refer [put! >!!]]
-            [clojure.pprint :refer [pprint]])
-  (:import (reactor.core.publisher Mono)
-           (discord4j.core.event.domain.message MessageCreateEvent)
-           (discord4j.core DiscordClient GatewayDiscordClient DiscordClientBuilder)
+            [clojure.core.async :refer [put!]])
+  (:import (discord4j.core.event.domain.message MessageCreateEvent)
+           (discord4j.core  GatewayDiscordClient DiscordClientBuilder)
            (discord4j.core.object.entity.channel TextChannel)
-           (discord4j.rest.util Image Image$Format)
-           (discord4j.core.spec UserEditSpec MessageCreateSpec)))
-
-(defmacro as-function [f & args]
-  `(reify java.util.function.Function
-     (apply [this arg#]
-       (~f arg# ~@args))))
-
-(defmacro as-runnable [f]
-  `(reify java.lang.Runnable
-     (run [this]
-       (~f))))
-
-(defmacro as-predicate [f]
-  `(reify java.util.function.Predicate
-     (test [this arg#]
-       (~f arg#))))
+           (discord4j.rest.util Image)
+           (discord4j.core.spec UserEditSpec)))
 
 (defmacro as-consumer [f]
   `(reify java.util.function.Consumer
@@ -64,48 +47,5 @@
       (doOnError (as-consumer (fn [error] (println error))))
       (subscribe (as-consumer (fn [event] (put! msg-chan (MessageCreateEvent->message event)))))))
 
-(defn message-filter [match]
-  (as-predicate (fn [message] (if (.equals match (.getContent message)) java.lang.Boolean/TRUE java.lang.Boolean/FALSE))))
-
 (defn set-bot-profile [^GatewayDiscordClient gateway image-path format nickname]
   (.. gateway (edit (as-consumer (fn [^UserEditSpec userEditSpec] (.. userEditSpec (setAvatar (Image/ofRaw (file->bytes image-path) format)) (setUsername nickname) (asRequest))))) (block)))
-
-(defn set-bot-profile-old [^DiscordClient client image-path format nickname]
-  (.. client (withGateway (as-function set-bot-profile image-path format nickname))))
-
-(defn MessageMono->message [message-handler message]
-  (let [channelMono (.getChannel message)]
-    (case (.getContent message)
-      "!ping" (.. channelMono
-                  (flatMap (as-function (fn [channel] (.createMessage channel "pong"))))
-                  (doOnError (as-consumer (fn [error] (println error)))))
-      "!texas" (do (.. channelMono
-                       (doOnError (as-consumer (fn [error] (println error)))))
-                   (.. channelMono
-                       (flatMap (as-function (fn [channel] (set-bot-profile "./resources/texas.png" Image$Format/PNG "Senator McConaughey"))))))
-      "!reset" (do (.. channelMono
-                       (doOnError (as-consumer (fn [error] (println error)))))
-                   (.. channelMono
-                       (flatMap (as-function (fn [channel] (set-bot-profile "./resources/BunsenHoneydew.jpg" Image$Format/JPEG "TestMuppet"))))))
-      (.. channelMono
-          (flatMap (as-function (fn [channel] (Mono/just message))))))))
-
-(defn MessageMono->MessageEvent [message-handler]
-  (as-function
-   (fn [event]
-     (.. Mono
-         (just (.getMessage event))
-         (flatMap (as-function (fn [message] (message-handler (.getContent message))
-                                 (.. (.getChannel message) (flatMap (as-function (fn [channel] (Mono/just message))))))))
-         (doOnError (as-consumer (fn [error] (println error))))))))
-
-(defn gateway->MessageMono [message-handler]
-  (as-function
-   (fn [gateway]
-     (let [message (.on gateway MessageCreateEvent (MessageMono->MessageEvent message-handler))
-           disconnect (.. gateway (onDisconnect) (doOnTerminate (as-runnable #(print "Disconnected!"))))]
-       (Mono/when [message disconnect])))))
-
-(defn listen! [client term-chan message-handler]
-  (.. client (withGateway (gateway->MessageMono message-handler)) (block))
-  (>!! term-chan "terminate"))
--- src/muppeteer/core.clj
+++ src/muppeteer/core.clj
@@ -1,70 +1,11 @@
 (ns muppeteer.core
   (:require [clojure.edn :as edn]
-            [clojurewerkz.meltdown.reactor :as mr]
-            [clojurewerkz.meltdown.selectors :refer [match-all $]]
-            [clojurewerkz.meltdown.streams :as ms :refer [create consume accept filter*]]
-            [clojure.pprint :refer [pprint]]
-            [muppeteer.bot :refer [create-client listen! start-main send-message set-bot-profile]]
+            [muppeteer.bot :refer [create-client start-main send-message set-bot-profile]]
             [clojure.core.async :refer [chan <! go-loop mult tap sliding-buffer]])
-  (:import (reactor.core.publisher Mono)
-           (discord4j.core.event.domain.message MessageCreateEvent)
-           (discord4j.core DiscordClient GatewayDiscordClient DiscordClientBuilder)
-           (discord4j.rest.util Image Image$Format)))
+  (:import (discord4j.core GatewayDiscordClient)
+           (discord4j.rest.util Image$Format)))
 
 (defonce config (edn/read-string (slurp "config.edn")))
-
-(defonce state (atom {}))
-
-(defn init-event-pump! [main-in])
-
-(defn init-reactor! []
-  (let [r (mr/create :dispatcher-type :thread-pool :event-routing-strategy :broadcast)
-        m (create)]
-    (swap! state assoc :reactor r)
-    (swap! state assoc :messages m)
-    ;; (mr/on r (match-all) (fn [event] (pprint event)))
-    ))
-
-(defn notify [k message]
-  (let [r (:reactor @state)]
-    (mr/notify r k message)))
-
-(defn on-exact [k f]
-  (let [r (:reactor @state)]
-    (mr/on r ($ k) f)))
-
-(defn main-listen [message]
-  (println message)
-  (notify :message message))
-
-(defn start-bot!
-  ([bot token listen]
-   (let [client (create-client token)
-         terminate (chan)]
-     (swap! state assoc bot {:client client :terminate-chan terminate})
-     (listen! client terminate listen)))
-
-  ([bot token listen terminate-chan]
-   (let [client (create-client token)]
-     (swap! state assoc bot {:client client :terminate-chan terminate-chan})
-     (listen! client terminate-chan listen))))
-
-(defn start-bot-sub [event]
-  (let [bot (get-in event [:data :bot])
-        token (get-in config [bot :token])
-        terminate-chan (get-in event [:data :terminate-chan])
-        listen (or (get-in event [:data :listen]) (fn []))]
-    (if terminate-chan
-      (start-bot! bot token listen terminate-chan)
-      (start-bot! bot token listen))))
-
-(defn init-subscribers []
-  (let [r (:reactor @state)]
-    (mr/on-error r Exception (fn [event]
-                               (println event)))
-    (mr/on r ($ :start-bot) (fn [event] (start-bot-sub event)))
-    (mr/on r ($ :debug-state) (fn [event] (println @state)))
-    (mr/on r ($ :message) (fn [message] (println message)))))
 
 (defn sliding-buffered-channel [buff]
   (chan (sliding-buffer buff)))
